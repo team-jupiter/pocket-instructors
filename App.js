0518c9923853f8c89c587d492c0f4331287e2ccc
Andrew Lee <anlee914@gmail.com>
2021-05-05 18:10:51 -0700

    saving first working version of rendered map

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Dimensions } from 'react-native';
+import {
+  Platform,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  Image
+} from 'react-native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import MapView from 'react-native-maps';
+import { generateRandomPoint } from './Utilities/locationGenerator';
+import { dummyData } from './startingdummy';
+
 export default function App() {
+  const targetRadius = 150;
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+
+
+
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -14,6 +28,8 @@ export default function App() {
         return;
       }
       let location = await Location.getCurrentPositionAsync({});
+      // need to refresh this entire thing frequently, e.g. something like below ---
+      // let location = await Location.watchPositionAsync({timeInterval: 1000})
       setLocation(location);
     })();
   }, []);
@@ -26,37 +42,161 @@ export default function App() {
   if (location === null || location === undefined) {
     return <View style={styles.container}></View>;
   } else {
+    //Going to make the call to firebase here ---- every ~5 seconds, I am going to destroy existing markers
+    //and create ~5 new Pokemon using the images that we have stored in the DB for each Pokemon
+
+    console.log('location is...', location);
+    // console.log('dummydata is ...', dummyData);
+    const userLocation = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 1 / 300,
+      longtitudeDelta: 2 / 300
+    };
+
+    let instructorTracker = [];
+    for (let i = 0; i < 15; i++) {
+      //generate random number btwn 1-10 ... or however many Pokemon there are .... eg Math.floor(Math.random() * 10)
+      //make API call to grab object for that user (e.g. firebase/pokemon/id/1)
+      const instructorLocation = generateRandomPoint(
+        userLocation,
+        targetRadius,
+        1
+      );
+
+      console.log('results of RNG code ....', instructorLocation);
+
+      let newObjToPush = {};
+
+      newObjToPush.longitude = instructorLocation.longitude;
+      newObjToPush.latitude = instructorLocation.latitude;
+      instructorTracker.push(newObjToPush);
+      console.log(
+        'interim instructorTracker results are ...',
+        instructorTracker
+      );
+    }
+
+    const instructorLocation = generateRandomPoint(
+      userLocation,
+      targetRadius,
+      1
+    );
+    console.log('userLocation is ...', userLocation);
+    console.log('instructorLocation is ...', instructorLocation);
+    // console.log('the entire instructorTracker array looks like ...', instructorTracker)
+    console.log(
+      'first instructorTracker in arr lat is....',
+      instructorTracker[0].latitude
+    );
+    // console.log('instructorLocation is ...', instructorLocation)
+
     return (
+      // somewhere here, I will need to be able to have a button or trigger where we can redirect to the
+      // battle/capture screen and pass in props where props is something like instructor ID .....
+
       <View style={styles.container}>
         <MapView
+          showsBuildings
+          ref={(ref) => {
+            this.map = ref;
+          }}
+          userInterfaceStyle='dark'
+          // customMapStyle = {mapStyle}
+          onLayout={() => {
+            this.map.animateToBearing(125);
+            this.map.animateToViewingAngle(45);
+          }}
           initialRegion={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
             latitudeDelta: 1 / 300,
-            longitudeDelta: 2 / 300,
+            longitudeDelta: 2 / 300
           }}
           style={styles.mapStyle}
-        />
+        >
+          {/* reate an array of randomly generated instuctors and then .map through each one */}
+
+          {/* <MapView.Marker
+            coordinate={{
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude
+            }}
+            image={require('./imgs/pic.png')}
+          /> */}
+
+          <MapView.Marker
+            // key={`${p.latitude}::${p.longitude}`}
+            coordinate={{
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude
+            }}
+          >
+            <Image
+              source={require('./imgs/pic.png')}
+              style={{ width: 40, height: 42 }}
+              resizeMode='contain'
+            />
+          </MapView.Marker>
+
+          {/* solomarker */}
+          {/* <MapView.Marker
+          coordinate={{latitude: instructorLocation.latitude,
+          longitude: instructorLocation.longitude}}
+          image={require('./imgs/steel.png')}
+        /> */}
+
+          {/* iterating through multiple markers */}
+          {/* {instructorTracker.map((eachInstructor, index) => {
+            console.log('eachInstructors lat is...', eachInstructor.latitude)
+            // <MapView.Marker
+            //   key={index}
+            //   coordinate={{
+            //     latitude: eachInstructor.latitude,
+            //     longitude: eachInstructor.longitude
+            //   }}
+            //   image={require('./imgs/steel.png')}
+            // />
+          })} */}
+
+          {/* THE THING BELOW IS THE ONLY THING THAT WORKS DONT FORGET!!!! */}
+          {instructorTracker.map((p) => (
+            <MapView.Marker
+              // key={`${p.latitude}::${p.longitude}`}
+              coordinate={{
+                latitude: p.latitude,
+                longitude: p.longitude
+              }}
+            >
+              <Image
+                source={require('./imgs/tanner.png')}
+                style={{ width: 40, height: 42 }}
+                resizeMode='contain'
+              />
+            </MapView.Marker>
+          ))}
+        </MapView>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
-    padding: 8,
+    padding: 8
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'center',
+    textAlign: 'center'
   },
   mapStyle: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+    height: Dimensions.get('window').height
+  }
 });
---
+++ startingseed.js
@@ -0,0 +1,110 @@
+Pokemon Table
+
+  {
+    instructorDexID: 1,
+    instructorName: 'Eric Katz',
+    description: 'Eric Katz AKA Prof. His insistence on wearing only white makes him vulnerable to coffee based attacks',
+    maxAttack: 50,
+    maxDefense: 20,
+    maxHP: 100,
+    moveSet: [
+      {
+        move: 'Use Vim instead of VSCode',
+        attack: 20,
+        type: 'Built Different'
+      },
+      {
+        move: 'Rub hair oil into beard',
+        attack: 15,
+        type: 'Built Different'
+      },
+      {
+        move: 'Reassign HELP ticket',
+        attack: 20,
+        type: 'Built Different'
+      }
+    ],
+    type: 'Built Different',
+    image: 'www.yahoo.com'
+  }
+
+
+  {
+    instructorDexID: 2,
+    instructorName: 'Jon Dagdagan',
+    description: 'Jon is a legendary instructor. His voice is said to bring peace to world wars and calamities',
+    maxAttack: 200,
+    maxDefense: 50,
+    maxHP: 500,
+    moveSet: [
+      {
+        move: 'Provides pictures for student projects',
+        attack: 200,
+        type: 'Built Different'
+      },
+      {
+        move: 'Closes HELP ticket',
+        attack: 150,
+        type: 'Educational'
+      },
+    ],
+    type: 'Educational',
+    image:
+  }
+
+
+Trainer Table
+  {
+    email: 'anlee914@gmail.com',
+    instructors: [
+      {
+        instructorDexID: 1,
+        instructorName: 'Eric Katz',
+        description: 'Eric Katz AKA Prof. His insistence on wearing only white makes him vulnerable to coffee based attacks',
+        attack: 42,
+        defense: 14,
+        hp: 15,
+        moveSet: [
+          {
+            move: 'Use Vim instead of VSCode',
+            attack: 20,
+            type: 'Built Different'
+          },
+          {
+            move: 'Rub hair oil into beard',
+            attack: 15,
+            type: 'Built Different'
+          },
+          {
+            move: 'Reassign HELP ticket',
+            attack: 20,
+            type: 'Built Different'
+          }
+        ],
+        type: 'Built Different',
+        image:
+      },
+      {
+        instructorDexID: 2,
+        instructorName: 'Jon Dagdagan',
+        description: 'Jon is a legendary instructor. His voice is said to bring peace to world wars and calamities',
+        attack: 199,
+        defense: 49,
+        hp: 420,
+        moveSet: [
+          {
+            move: 'Provides pictures for student projects',
+            attack: 200,
+            type: 'Built Different'
+          },
+          {
+            move: 'Closes HELP ticket',
+            attack: 150,
+            type: 'Educational'
+          },
+        ],
+        type: 'Educational',
+        image:
+      }
+    ]
+  }
